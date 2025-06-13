d8381527b09fdf62cdcfae792a3a5760e6019a1d
Steven-422024001 <steven.422024001@civitas.ukrida.ac.id>
2025-06-13 14:47:00 +0700

    auth passport

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'passport', //gunakan password API guard
+            'provider' => 'users',  
+        ]       
     ],
 
     /*
